b672aa3d36bc17a167d461223b90adb65f781be0
Roman Lesnoy <romanlesnoy@outlook.com>
2022-07-15 17:03:08 +0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
-    return <div></div>;
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Redirect to="/quotes" />
+            </Route>
+            <Route exact path="/quotes">
+                <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+                <QuoteDetail />
+            </Route>
+            <Route path="/new-quote">
+                <NewQuote />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
