b3e52087ad6be8d1f7200db0a3cb53bf276ffc0f
Roman Lesnoy <romanlesnoy@outlook.com>
2022-07-16 18:00:56 +0300

    Get single quote data from api

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_DATA = [
-    { id: "q1", author: "Max", text: "React is fun" },
-    { id: "q2", author: "Max", text: "React is great" }
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
     const match = useRouteMatch();
     const params = useParams();
 
-    const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+    const { quoteId } = params;
 
-    if (!quote) {
-        return <p>No quote found</p>;
+    const {
+        sendRequest,
+        status,
+        data: loadedQuote,
+        error
+    } = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    if (status === "pending") {
+        return (
+            <div className="centered">
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className="centered focused">{error}</p>;
+    }
+
+    if (!loadedQuote.text) {
+        return <p className="centered focused">No quote found!</p>;
     }
 
     return (
         <section>
-            <HighlightedQuote text={quote.text} author={quote.author} />
-
+            <HighlightedQuote
+                text={loadedQuote.text}
+                author={loadedQuote.author}
+            />
             <Route exact path={match.path}>
                 <div className="centered">
                     <Link to={`${match.url}/comments`} className="btn--flat">
@@ -30,7 +53,6 @@ const QuoteDetail = () => {
                     </Link>
                 </div>
             </Route>
-
             <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
