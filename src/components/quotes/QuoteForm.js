53db1e41e27b7b7b565c596bf3375b2d246df093
Roman Lesnoy <romanlesnoy@outlook.com>
2022-07-16 16:42:55 +0300

    Add a Propt component to prevent form data loss on redirect

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,11 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, Fragment } from "react";
+import { Prompt } from "react-router-dom";
 import PropTypes from "prop-types";
 
-import Card from "../ui/Card";
-import LoadingSpinner from "../ui/LoadingSpinner";
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+    const [isEntering, setIsEntering] = useState(false);
+
     const authorInputRef = useRef();
     const textInputRef = useRef();
 
@@ -20,34 +23,63 @@ const QuoteForm = (props) => {
         props.onAddQuote({ author: enteredAuthor, text: enteredText });
     }
 
-    return (
-        <Card>
-            <form className={classes.form} onSubmit={submitFormHandler}>
-                {props.isLoading && (
-                    <div className={classes.loading}>
-                        <LoadingSpinner />
-                    </div>
-                )}
+    const finishingEnteringHandler = () => {
+        setIsEntering(false);
+    };
 
-                <div className={classes.control}>
-                    <label htmlFor="author">Author</label>
-                    <input type="text" id="author" ref={authorInputRef} />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="text">Text</label>
-                    <textarea id="text" rows="5" ref={textInputRef}></textarea>
-                </div>
-                <div className={classes.actions}>
-                    <button className="btn">Add Quote</button>
-                </div>
-            </form>
-        </Card>
+    const formFocusHandler = () => {
+        setIsEntering(true);
+    };
+
+    return (
+        <Fragment>
+            <Prompt
+                when={isEntering}
+                message={() =>
+                    "Are your sure you want to leave? All your entered data will be lost?"
+                }
+            />
+            <Card>
+                <form
+                    className={classes.form}
+                    onSubmit={submitFormHandler}
+                    onFocus={formFocusHandler}
+                >
+                    {props.isLoading && (
+                        <div className={classes.loading}>
+                            <LoadingSpinner />
+                        </div>
+                    )}
+
+                    <div className={classes.control}>
+                        <label htmlFor="author">Author</label>
+                        <input type="text" id="author" ref={authorInputRef} />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="text">Text</label>
+                        <textarea
+                            id="text"
+                            rows="5"
+                            ref={textInputRef}
+                        ></textarea>
+                    </div>
+                    <div className={classes.actions}>
+                        <button
+                            onClick={finishingEnteringHandler}
+                            className="btn"
+                        >
+                            Add Quote
+                        </button>
+                    </div>
+                </form>
+            </Card>
+        </Fragment>
     );
 };
 
 QuoteForm.propTypes = {
     onAddQuote: PropTypes.func.isRequired,
-    isLoading: PropTypes.bool.isRequired
+    isLoading: PropTypes.bool
 };
 
 export default QuoteForm;
