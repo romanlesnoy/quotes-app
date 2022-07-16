149f76197913bc7d53636fcf14a7b5dc1ba3e6c0
Roman Lesnoy <romanlesnoy@outlook.com>
2022-07-16 16:38:44 +0300

    Add sort logic and query params for it

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,14 +1,43 @@
 import React, { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+    return quotes.sort((quoteA, quoteB) => {
+        if (ascending) {
+            return quoteA.id > quoteB.id ? 1 : -1;
+        } else {
+            return quoteA.id < quoteB.id ? 1 : -1;
+        }
+    });
+};
+
 const QuoteList = (props) => {
+    const history = useHistory();
+    const location = useLocation();
+
+    const queeryParams = new URLSearchParams(location.search);
+
+    const isSortingAscending = queeryParams.get("sort") === "asc";
+
+    const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+    const ChangeSortingHandler = () => {
+        history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+    };
+
     return (
         <Fragment>
+            <div className={classes.sorting}>
+                <button onClick={ChangeSortingHandler}>
+                    Sorting {isSortingAscending ? "Discending" : "Ascending"}
+                </button>
+            </div>
             <ul className={classes.list}>
-                {props.quotes.map((quote) => (
+                {sortedQuotes.map((quote) => (
                     <QuoteItem
                         key={quote.id}
                         id={quote.id}
